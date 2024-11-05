5bbd3cd8ab52e6597363c1e70a55c9b67d4c2b22
Yash Gupta <yashgupta@gkmit.co>
2024-11-05 17:02:27 +0530

    feat: [bms-3] database setup with postgres using sequelize

---
+++ src/config/config.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = 
+{
+  "development": {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": "postgres"
+  },
+  "test": {
+     "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": "postgres"
+  }
+}
+
