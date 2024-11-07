958c358b4efad2d555c14c205338b729e56781b3
Yash Gupta <yashgupta@gkmit.co>
2024-11-07 13:08:15 +0530

    feat:[bms-4] husky setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,17 +7,13 @@ export default [
       ecmaVersion: "latest",
       sourceType: "module",
       globals: {
-        ...globals.browser, // For browser global variables
         ...globals.node, // For Node.js global variables
       },
     },
     rules: {
-      "no-unused-vars": "warn", // Warn on unused variables
-      "no-console": "warn", // Warn on console.log usage
+      "no-unused-vars": "error", // Warn on unused variables
       eqeqeq: "error", // Require === and !==
       semi: ["error", "always"], // Enforce semicolons at the end of statements
-      //"quotes": ["error", "double"], // Use single quotes
-      //"indent": ["error", 2],      // Enforce 2-space indentation
     },
   },
 ];
