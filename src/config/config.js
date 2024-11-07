6efd2944b970460af950d098a1d31058f5895357
Yash Gupta <yashgupta@gkmit.co>
2024-11-07 12:28:57 +0530

    fix:[bms-3] .env.sample and config.js file

--- src/config/config.js
+++ src/config/config.js
@@ -4,18 +4,18 @@ dotenv.config();
 module.exports = 
 {
   "development": {
-    "username": process.env.USERNAME,
-    "password": process.env.PASSWORD,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
     "database": process.env.DATABASE,
-    "host": process.env.HOST,
-    "dialect": "postgres"
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
   },
   "test": {
-     "username": process.env.USERNAME,
-    "password": process.env.PASSWORD,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
     "database": process.env.DATABASE,
-    "host": process.env.HOST,
-    "dialect": "postgres"
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
   }
 }
 
