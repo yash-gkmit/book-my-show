6fa9b0114901cb962d9f39cc8d2637644dc9a2f1
Yash Gupta <yashgupta@gkmit.co>
2024-11-07 12:37:28 +0530

    fix:[bms-3] add changes in env

--- src/config/config.js
+++ src/config/config.js
@@ -6,14 +6,14 @@ module.exports =
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DATABASE,
+    "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT
   },
   "test": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DATABASE,
+    "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT
   }
