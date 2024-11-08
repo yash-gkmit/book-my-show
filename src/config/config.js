0400f0bbe0f47213792853429606323cd92c15d4
Yash Gupta <yashgupta@gkmit.co>
2024-11-08 15:11:08 +0530

    feat:[bms-6] add uuid-extension migration

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
 const dotenv = require('dotenv');
-dotenv.config();
+dotenv.config({ path: __dirname + '/../../.env' });
 
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    password: `${process.env.DB_PASSWORD}`,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'postgres',
   },
   test: {
     username: process.env.DB_USERNAME,
