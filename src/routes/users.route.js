5b53d46c6b5217c22b471bc1d10de7d420958617
Yash Gupta <yashgupta@gkmit.co>
2024-11-11 23:04:01 +0530

    fix:[bms-11] fix route

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const {
-	fetchAll,
-	fetchById,
-	change,
-	remove,
+  fetchAll,
+  fetchById,
+  change,
+  remove,
 } = require('../controllers/users.controller');
 const { authMiddleware } = require('../middlewares/auth.middleware');
 const { rbacMiddleware } = require('../middlewares/rbac.middleware');
@@ -13,17 +13,17 @@ const router = express.Router();
 router.get('/', authMiddleware, rbacMiddleware(['Admin']), fetchAll);
 
 router.get(
-	'/:user_id',
-	authMiddleware,
-	rbacMiddleware(['Admin', 'self']),
-	fetchById,
+  '/:user_id',
+  authMiddleware,
+  rbacMiddleware(['Admin', 'self']),
+  fetchById,
 );
 
 router.put(
-	'/:user_id',
-	authMiddleware,
-	rbacMiddleware(['Admin', 'self']),
-	change,
+  '/:user_id',
+  authMiddleware,
+  rbacMiddleware(['Admin', 'self']),
+  change,
 );
 
 router.delete('/:user_id', authMiddleware, rbacMiddleware(['Admin']), remove);
