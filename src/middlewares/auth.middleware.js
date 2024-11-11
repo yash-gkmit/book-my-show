f692193384ba1f881f989082177e1a70f9826d4b
Yash Gupta <yashgupta@gkmit.co>
2024-11-11 15:22:26 +0530

    fix: [bms-9] add middleware for login

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,23 +3,23 @@ const { isTokenBlacklisted } = require('../helpers/redis.helper');
 const { verifyToken } = require('../helpers/jwt.helper');
 
 exports.authMiddleware = async (req, res, next) => {
-	const token = req.headers['authorization']?.split(' ')[1];
+  const token = req.headers['authorization']?.split(' ')[1];
 
-	if (!token) {
-		throwCustomError('Token is required', 401);
-	}
+  if (!token) {
+    throwCustomError('Token is required', 401);
+  }
 
-	try {
-		const isBlacklisted = await isTokenBlacklisted(token);
-		if (isBlacklisted) {
-			throwCustomError('Token is blacklisted', 401);
-		}
+  try {
+    const isBlacklisted = await isTokenBlacklisted(token);
+    if (isBlacklisted) {
+      throwCustomError('Token is blacklisted', 401);
+    }
 
-		const decoded = await verifyToken(token);
-		req.user = decoded;
-		next();
-	} catch (error) {
-		console.log(error);
-		throwCustomError('Unauthorized', 401);
-	}
+    const decoded = await verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.log(error);
+    throwCustomError('Unauthorized', 401);
+  }
 };
