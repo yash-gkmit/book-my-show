92f854944ceb3cc1debdbc84ce0addf5224d21a2
Yash Gupta <yashgupta@gkmit.co>
2024-11-12 14:44:51 +0530

    feat:[bms-14] add multer setup

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const uploadPath = path.join(__dirname, '../uploads');
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
